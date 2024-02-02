58a5d0cbaad0277fda46901c3785bae584adaa9d
pr0mming <pr0mming@users.noreply.github.com>
2024-02-01 20:15:45 -0500

    Add solution for problem 6 part 1

--- lib/functions/problems_factory.go
+++ lib/functions/problems_factory.go
@@ -16,6 +16,7 @@ import (
 	problems_4_2 "aoc.2023/problems/4/part-2"
 	problems_5_1 "aoc.2023/problems/5/part-1"
 	problems_5_2 "aoc.2023/problems/5/part-2"
+	problems_6_1 "aoc.2023/problems/6/part-1"
 )
 
 // This "Factory Method" triggers the problem solution
@@ -50,6 +51,8 @@ func SolveProblemByKey(args []string) (string, error) {
 		answer = problems_5_1.SolveChallenge(problemId)
 	case "52":
 		answer = problems_5_2.SolveChallenge(problemId)
+	case "61":
+		answer = problems_6_1.SolveChallenge(problemId)
 	default:
 		return "", errors.New("The given args aren not in a valid range, try something like: [1 1]")
 	}
---
+++ problems/6/part-1/6.go
@@ -0,0 +1,69 @@
+package problems_6_1
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	common_functions "aoc.2023/lib/common/functions"
+)
+
+func SolveChallenge(problemId string) string {
+	// Process the input
+	inputFilePath := fmt.Sprintf("problems/%s/input.txt", problemId)
+	scanner := common_functions.CreateInputScanner(inputFilePath)
+	defer scanner.File.Close()
+
+	// Keep the values in the same format (time-distance) in an array (2 rows)
+	var paperSheet [2][]int
+	i := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		separatorIndex := strings.Index(line, ":")
+		valuesStr := strings.Fields(line[separatorIndex+1:])
+
+		paperSheet[i] = common_functions.GetIntegersArr(valuesStr, false)
+		i++
+	}
+
+	// Use it to keep the final multiplication
+	answer := 1
+
+	// Iterate over the columns
+	for i := 0; i < len(paperSheet[0]); i++ {
+		time := paperSheet[0][i]
+		distance := paperSheet[1][i]
+
+		// With the half we start with the max distance instead the beginning
+		timeHalf := int(math.Floor(float64(time) / 2))
+
+		// Use it to keep the number of max distances
+		records := 0
+
+		// Do the sequence from the max until the min distance (> distance)
+		for x := timeHalf; ; x-- {
+			multiplier := time - x
+
+			if x*multiplier > distance {
+				records++
+			} else {
+				break
+			}
+		}
+
+		// We add up the double of the records it's a mirror "pattern"
+		records *= 2
+
+		// With even numbers we need minus 1
+		if time%2 == 0 {
+			records--
+		}
+
+		answer *= records
+	}
+
+	return strconv.Itoa(answer)
+}
