ffd80353559504bf346be5588294933265314921
pr0mming <pr0mming@users.noreply.github.com>
2024-02-12 19:01:07 -0500

    Add solution to problem 11 part 2

--- lib/functions/problems_factory.go
+++ lib/functions/problems_factory.go
@@ -11,6 +11,7 @@ import (
 	problems_10_1 "aoc.2023/problems/10/part-1"
 	problems_10_2 "aoc.2023/problems/10/part-2"
 	problems_11_1 "aoc.2023/problems/11/part-1"
+	problems_11_2 "aoc.2023/problems/11/part-2"
 	problems_2_1 "aoc.2023/problems/2/part-1"
 	problems_2_2 "aoc.2023/problems/2/part-2"
 	problems_3_1 "aoc.2023/problems/3/part-1"
@@ -83,6 +84,8 @@ func SolveProblemByKey(args []string) (string, error) {
 		answer = problems_10_2.SolveChallenge(problemId)
 	case "111":
 		answer = problems_11_1.SolveChallenge(problemId)
+	case "112":
+		answer = problems_11_2.SolveChallenge(problemId)
 	default:
 		return "", errors.New("The given args aren not in a valid range, try something like: [1 1]")
 	}
---
+++ problems/11/part-2/11.go
@@ -0,0 +1,108 @@
+package problems_11_2
+
+import (
+	"fmt"
+	"strconv"
+
+	common_functions "aoc.2023/lib/common/functions"
+	common_types "aoc.2023/lib/common/types"
+)
+
+var GALAXY_CHAR = []byte("#")[0]
+
+const GALAXY_EXPANSION_PARAMMETER = (1000000 - 1) // Set how big is the expanded space
+
+func SolveChallenge(problemId string) string {
+	// Process the input
+	inputFilePath := fmt.Sprintf("problems/%s/input.txt", problemId)
+	scanner := common_functions.CreateInputScanner(inputFilePath)
+	defer scanner.File.Close()
+
+	// galaxiesPos, is the list of positions of galaxies [X, Y]
+	// nonExpandedRows, is a list to represent the rows of the map (like a map[int]bool) to check if the row is expanded
+	// nonExpandedColumns, the same above but with columns
+	galaxiesPos, nonExpandedRows, nonExpandedColumns := processImageInput(*scanner)
+	var answer int64 = 0
+
+	// Loop to form the couples
+	for i := 0; i < len(galaxiesPos); i++ {
+		for j := i + 1; j < len(galaxiesPos); j++ {
+
+			pointA := galaxiesPos[i]
+			pointB := galaxiesPos[j]
+
+			rowsSteps := getExpandedPaths(nonExpandedRows, pointA[0], pointB[0])
+			colsSteps := getExpandedPaths(nonExpandedColumns, pointA[1], pointB[1])
+
+			answer += rowsSteps + colsSteps
+		}
+	}
+
+	return strconv.FormatInt(answer, 10)
+}
+
+func getExpandedPaths(paths []bool, a, b int) int64 {
+	var (
+		rowsOffset    int64 = 0
+		expandedPaths int64 = 0
+	)
+
+	// Avoid get negative numbers
+	if a > b {
+		aTmp := a
+		a = b
+		b = aTmp
+	}
+
+	rowsOffset = int64(b - a)
+
+	// Check extra steps (expanded paths for rows or columns)
+	for i := a + 1; i < b; i++ {
+		if !paths[i] {
+			expandedPaths++
+		}
+	}
+
+	expandedPaths *= GALAXY_EXPANSION_PARAMMETER
+
+	return rowsOffset + expandedPaths
+}
+
+func processImageInput(scanner common_types.FileInputScanner) ([][2]int, []bool, []bool) {
+	var (
+		galaxiesPos [][2]int     // Is the list of positions of galaxies [X, Y]
+		rows        int      = 0 // Used to save the number of rows of the map
+		cols        int      = 0 // Used to save the number of columns of the map
+	)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		cols = len(line)
+
+		// Loop to save the galaxies
+		for j := 0; j < len(line); j++ {
+			if line[j] == GALAXY_CHAR {
+				galaxiesPos = append(galaxiesPos, [2]int{rows, j})
+			}
+		}
+
+		rows++
+	}
+
+	// Now we make a representation for rows and columns like a map[int]bool
+	// This is used to calculate the extra steps between galaxies
+	var (
+		nonExpandedRows    = make([]bool, rows)
+		nonExpandedColumns = make([]bool, cols)
+	)
+
+	for _, galaxyPos := range galaxiesPos {
+		row := galaxyPos[0]
+		col := galaxyPos[1]
+
+		nonExpandedRows[row] = true
+		nonExpandedColumns[col] = true
+	}
+
+	return galaxiesPos, nonExpandedRows, nonExpandedColumns
+}
